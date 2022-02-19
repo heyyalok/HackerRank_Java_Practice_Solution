ae372cbcf410745c3a4b6c03411eb30c5cb4dd65
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-19 22:05:53 +0530

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,11 @@
+public class Multiplicationtable
+{
+    public static void main(String[]args)
+    {
+        int n = 2;
+        for (int i=1; i<=10;i++)
+        {
+            System.out.println(n+" x "+i+" = "+(n*i));
+        }
+    }
+}
