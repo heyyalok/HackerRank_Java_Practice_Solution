e67f42fd17d0337aa7141db5a46c2a3b9746841e
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-19 22:11:05 +0530

    Update Java Loops I.java

--- Java Loops I.java
+++ Java Loops I.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Multiplicationtable
 {
     public static void main(String[]args)
     {
-        int n = 2;
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
         for (int i=1; i<=10;i++)
         {
             System.out.println(n+" x "+i+" = "+(n*i));
